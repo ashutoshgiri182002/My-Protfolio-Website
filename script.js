275ac2fbf0c0414baef012896d5dd05adf1439e0
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2022-06-10 16:21:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function sendMail(e){
+
+    e.preventDefault();
+
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const phoneno = document.querySelector('#phoneno').value;
+    const message = document.querySelector('#message').value;
+
+    //console.log("Details: ",name,email,phoneno,message);
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "ashutoshgiri1803@gmail.com",
+        Password : "824AB621265B60FA0E0102D52B845E206CE9",
+        To : 'ashutoshgiri1803@gmail.com',
+        From : 'ashutoshgiri1803@gmail.com',
+        Subject : "😇Someone Contact on Your Portfolio Website.",
+        Body :  `Name: ${name} \nEmail: ${email} \nPhone Number: ${phoneno} \nMessage: ${message}`,
+    })
+    .then(response => {
+        if(response=="OK"){
+            alert("😃📩Mail Sent Successfully!");
+        }
+        else{
+            alert("😢Error! Mail Not Sent");
+        }
+    })
+}
+
+
+document.querySelector('form').addEventListener('submit',sendMail);
